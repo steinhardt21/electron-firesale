9022f602383dabcb3fb5263acfb92a2e0efbf363
steinhardt21 <alexandru.enache@protonmail.com>
2024-04-30 08:56:30 +0300

    refactor: the connection between the render and the main process is more granular

--- src/preload.ts
+++ src/preload.ts
@@ -2,12 +2,17 @@ import { ipcRenderer, contextBridge } from 'electron';
 import Elements from './renderer/elements';
 import { renderMarkdown } from './renderer/markdown';
 
-ipcRenderer.on('file-opened', (_, content: string) => {
-  Elements.MarkdownView.value = content;
-  renderMarkdown(content);
-});
+// ipcRenderer.on('file-opened', (_, content: string) => {
+//   Elements.MarkdownView.value = content;
+//   renderMarkdown(content);
+// });
 
 contextBridge.exposeInMainWorld('api', {
+  onFileOpen: (callback: (content: string) => string) => {
+    ipcRenderer.on('file-opened', (_, content: string) => {
+      callback(content)
+    })
+  },
   showOpenDialog: () => {
     ipcRenderer.send('show-open-dialog');
   },
